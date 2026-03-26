78e4484a2445b6e5ab742b358a8610657742d805
zhanggen <gen.zhang@yl.com>
2026-03-26 15:53:50 +0800

    优化

--- src/components/music-switch/MSHero.vue
+++ src/components/music-switch/MSHero.vue
@@ -163,7 +163,7 @@ onUnmounted(() => {
           transition: 'all 0.75s cubic-bezier(0.16, 1, 0.3, 1) 0.48s',
         }"
       >
-        <ProductHuntBadge theme="light" />
+        <ProductHuntBadge theme="light" slug="music-switch" post-id="1107592" />
       </div>
 
       <!-- badges -->
